f2c00e6cb8f1dea4d0e5adfdec86e25436fb802c
csloo <csloo180@gmail.com>
2016-03-11 17:21:16 -0800

    Create activity_picker.py

---
+++ activity_picker.py
@@ -0,0 +1,15 @@
+#This is my first program
+
+
+#import random picker
+import random
+
+# list of all the things to radomize
+my_list = ['dog', 'orange', 'cat', 'bear']
+
+# pick something
+picked_item = random.choice(my_list)
+
+# show user the selection
+
+print picked_item
